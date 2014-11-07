fbdfd38abfb97f76418b776156aa167277dbd06e
Nat <kaitomsuk@gmail.com>
2014-11-07 16:27:13 +0700

    testGetCaptchaLeftOperandShouldBe1

--- CaptchaServiceTest.php
+++ CaptchaServiceTest.php
@@ -1,28 +1,31 @@
 <?php
 require "CaptchaTest.php";
+require "RandomizerTestCase.php";
 
 class CaptchaService
 {
 	function setRandomizer( $randomizer )
 	{
-		
+		$this->randomizer = $randomizer;
 	}
 
 	function getCaptcha()
 	{
-		return new Captcha( 1,1,1,1 );
+		return new Captcha( 1, $this->randomizer->getRandomOperand(),1,1 );
 	}
 }
 
 class CaptchaServiceTest extends PHPUnit_Framework_TestCase
 {
-	public function testGetCaptcha()
+	public function testGetCaptchaLeftOperandShouldBe1()
 	{
 		$captchaService = new CaptchaService();
 		$stubRandomizer = $this->getMock('Randomizer');
 
-		$stubRandomizer->method('getRandomOperand')
-             ->willReturn(1);
+		$stubRandomizer
+			->expects($this->once())
+			->method('getRandomOperand')
+            ->willReturn(1);
 
 		$captchaService->setRandomizer( $stubRandomizer );
 		$this->assertEquals("1" , $captchaService->getCaptcha()->getLeftOperand() );

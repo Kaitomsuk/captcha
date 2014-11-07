19ecd96b4e6753193b506fec2ce1bcb15a84d555
Nat <kaitomsuk@gmail.com>
2014-11-07 12:02:49 +0700

    testPatterntestPattern

--- CaptchaTest.php
+++ CaptchaTest.php
@@ -1,6 +1,14 @@
 <?php
 
 class Captcha {
+	var $numstr =  array(
+		'Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine'
+	);
+
+	private function num2str($num) {
+		return $this->numstr[$num];
+	}
+
 	public function __construct($pattern, $leftOperand, $operator, $rightOperand) {
 		$this->leftOperand = $leftOperand;
 		$this->rightOperand = $rightOperand;
@@ -12,7 +20,14 @@ class Captcha {
 	}
 
 	public function getRightOperand() {
-		return $this->rightOperand;
+		if ($this->operator == 1) {
+			$rightOperand = $this->num2str($this->rightOperand);
+		}
+
+		else {
+			$rightOperand = $this->rightOperand;
+		}
+		return $rightOperand;
 	}
 
 	public function getOperator(){
@@ -34,12 +49,12 @@ class CaptchaTest extends PHPUnit_Framework_TestCase {
 	
 	function testRightOperandBe1WhenInputIs1111() {
 		$captcha = new Captcha(1,1,1,1);
-		$this->assertEquals("1", $captcha->getRightOperand());	
+		$this->assertEquals("One", $captcha->getRightOperand());	
 	}
 
 	function testRightOperandBe7WhenInputIs1117() {
 		$captcha = new Captcha(1,1,1,7);
-		$this->assertEquals("7", $captcha->getRightOperand());	
+		$this->assertEquals("Seven", $captcha->getRightOperand());	
 	}
 
 	function testOperatorBe1WhenInputIs1111() {
@@ -58,7 +73,8 @@ class CaptchaTest extends PHPUnit_Framework_TestCase {
 	}
 
 	function testPatternBe1WhenInputIs1111() {
-		$this->assertTrue(0);
+		$captcha = new Captcha(1,1,1,1);
+		$this->assertEquals("One", $captcha->getRightOperand());
 	}
 
 
